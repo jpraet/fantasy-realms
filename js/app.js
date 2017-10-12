fd1be86ae06afa50768c83480e7610cbfe57f6c6
Jimmy Praet <jimmy.praet@ksz-bcss.fgov.be>
2017-10-12 21:39:59 +0200

    add clear hand button + sound effects

--- js/app.js
+++ js/app.js
@@ -7,15 +7,30 @@ $(document).ready(function() {
   getHandFromQueryString();
 });
 
+var click = new Audio('sound/click.mp3');
+var swoosh = new Audio('sound/swoosh.mp3');
+var clear = new Audio('sound/clear.mp3');
+
 var hand = {};
 
+function clearHand() {
+  clear.play();
+  hand = {};
+  history.replaceState(null, null, "index.html");
+  updateHandView();
+}
+
 function addToHand(id) {
+  if (hand[id] === undefined) {
+    click.play();
+  }
   hand[id] = getCard(id);
   history.replaceState(null, null, "index.html?hand=" + Object.keys(hand).join());
   updateHandView();
 }
 
 function removeFromHand(id) {
+  swoosh.play();
   delete hand[id];
   if (hand.length > 0) {
     history.replaceState(null, null, "index.html?hand=" + Object.keys(hand).join());
