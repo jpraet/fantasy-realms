0209814d85ba3f2d4ad3a94b05f8b95302529cd5
Jimmy Praet <jimmy.praet@telenet.be>
2021-04-13 20:09:34 +0200

    add test "Collector does not score duplicated cards"

--- js/tests.js
+++ js/tests.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
   assertScoreByCode('9,12,16,37+9:16', 95, 'Island can be used even when blanked');
   assertScoreByCode('10,53+53:10', 23, 'Elementals count their Doppelgänger');
   assertScoreByCode('26,27,30,36,38,39,40+', 193, 'Collector can score multiple sets');
+  assertScoreByCode('26,46,47,51,53+51:46,53:46', 20, 'Collector does not score duplicated cards');
   assertScoreByCode('22,31,36,43,44,46,47+', 206, 'Gem of Order can score multiple sets')
 });
 
